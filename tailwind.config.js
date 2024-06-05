f998336535b755cecd5c7f54d10d8afece3842e0
mobina <mobinaeslamnia81@gmail.com>
2024-06-05 22:43:17 +0330

    version-1

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  darkMode:"class",
+  theme: {
+    screens: {
+      "mb": "380px",
+      "dt": "1024px"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
